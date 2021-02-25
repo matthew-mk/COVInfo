b98346296f49a70094200027a76df0ebfc9d8780
mkb18177 <jonathan.kerr.2018@uni.strath.ac.uk>
2021-02-25 19:52:06 +0000

    Fixed bug where view.js was trying to update id's on the profile page when not on that page anymore

--- view.js
+++ view.js
@@ -10,25 +10,33 @@ class View{
 
     // when called by controller will update the number of new cases in glasgow
     updateUserDefinedLocationNewCases(cases) {
-        console.log("got: ", cases)
-        document.getElementById("user-defined-location-new-cases").innerText = cases;
+        if(document.getElementById("user-defined-location-new-cases")!=null){ //checks that the page actually has the div so it doesnt try it on the wrong page
+            document.getElementById("user-defined-location-new-cases").innerText = cases;
+          }
     }
 
     updateUserDefinedLocationNewDeaths(cases) {
-        console.log("got: ", cases)
-        document.getElementById("user-defined-location-new-deaths").innerText = cases;
+        if(document.getElementById("user-defined-location-new-deaths")!=null){
+            document.getElementById("user-defined-location-new-deaths").innerText = cases;
+        }
     }
 
     updateNationalNewCases(cases){
-        document.getElementById("national-new-cases").innerText = cases;
+        if(document.getElementById("national-new-cases")!=null){
+            document.getElementById("national-new-cases").innerText = cases;
+        }
     }
 
     updateNationalNewDeaths(cases){
-        document.getElementById("national-new-deaths").innerText = cases;
+        if(document.getElementById("national-new-deaths")!=null){
+            document.getElementById("national-new-deaths").innerText = cases;
+        }
     }
 
     updateFirstDoseVaccinated(total){
-        document.getElementById("first-dose-vaccinated").innerText = total;
+        if(document.getElementById("first-dose-vaccinated")!=null){
+            document.getElementById("first-dose-vaccinated").innerText = total;
+        }
     }
 
 
