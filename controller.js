98873572ba3dfcda351cf3cab81a56a776d0087f
Matthew <MatthewMK108@gmail.com>
2021-03-16 23:38:10 +0000

    Added symptoms form functionality. The results of the symptoms form are displayed in certain areas of the home page and details page.

--- controller.js
+++ controller.js
@@ -59,6 +59,205 @@ const initialise = evt => {
     initStats();
 
     //EVENT LISTENERS
+    //Index page
+    if (document.URL.includes("index.html")) {
+        const symptomsButton = document.getElementById("symptoms-btn");
+
+        let numberOfSymptoms = JSON.parse(localStorage.getItem("lastSymptomsCheckResults")).length;
+        if (numberOfSymptoms > 0) {
+            symptomsButton.textContent = `${numberOfSymptoms} symptoms`;
+            symptomsButton.style.backgroundColor = "#f8c4c1";
+            symptomsButton.style.color = "#e96b64";
+        }
+    }
+
+    //Symptoms form page
+    if (document.URL.includes("symptomsform.html")) {
+        const symptomsFormDiv = document.getElementById("symptoms-form-container");
+        const symptomsResultsDiv = document.getElementById("symptoms-results-container");
+        const positiveResultsDiv = document.getElementById("display-positive-results");
+        const negativeResultsDiv = document.getElementById("display-negative-results");
+        const invalidInput = document.getElementById("invalid-input");
+        const submitButton = document.querySelector(".submit-button");
+        const clearButton = document.querySelector(".clear-button");
+        const highTemperature = document.getElementById("highTemperature");
+        const cough = document.getElementById("cough");
+        const lossOfSmell = document.getElementById("lossOfSmell");
+        const noneOfTheAbove = document.getElementById("noneOfTheAbove");
+        const breathingYes = document.getElementById("breathingYes");
+        const breathingNo = document.getElementById("breathingNo");
+
+        const clearFormInput = function () {
+            highTemperature.checked = false;
+            cough.checked = false;
+            lossOfSmell.checked = false;
+            noneOfTheAbove.checked = false;
+            breathingYes.checked = false;
+            breathingNo.checked = false;
+            invalidInput.textContent = "";
+        };
+
+        const isFormInputValid = function () {
+            if ((highTemperature.checked || cough.checked || lossOfSmell.checked || noneOfTheAbove.checked) && (breathingYes.checked  || breathingNo.checked)) {
+                return true;
+            }
+            return false;
+        };
+
+        const getSymptoms = function () {
+            let symptoms = [];
+            if (highTemperature.checked) {
+                symptoms.push("High temperature (fever)");
+            }
+            if (cough.checked) {
+                symptoms.push("A new, continuous cough");
+            }
+            if (lossOfSmell.checked) {
+                symptoms.push("A loss of smell, or a loss of taste");
+            }
+            if (breathingYes.checked) {
+                symptoms.push("Struggling to breath");
+            }
+            return symptoms;
+        };
+
+        const isFormInputPositive = function () {
+            if (getSymptoms().length > 0) {
+                return false;
+            }
+            return true;
+        };
+
+        submitButton.addEventListener("click", () => {
+            if (isFormInputValid()) {
+                //Valid input
+
+                //Update symptoms check history
+                if (localStorage.getItem("symptomsCheckHistory") === null) {
+                    //First time user is doing a check
+                    localStorage.setItem("lastSymptomsCheckResults", JSON.stringify(getSymptoms()));
+                    localStorage.setItem("lastSymptomsCheckDate", model.getFullDate());
+                    let lastSymptomsCheckNumSymptoms = JSON.parse(localStorage.getItem("lastSymptomsCheckResults")).length;
+                    let lastSymptomsCheckDate = localStorage.getItem("lastSymptomsCheckDate");
+                    let historyObject = [{"numberOfSymptoms": lastSymptomsCheckNumSymptoms, "date": lastSymptomsCheckDate}];
+                    localStorage.setItem("symptomsCheckHistory", JSON.stringify(historyObject));
+                } else {
+                    //User has done checks before, so append the results of this check to the history
+                    localStorage.setItem("lastSymptomsCheckResults", JSON.stringify(getSymptoms()));
+                    localStorage.setItem("lastSymptomsCheckDate", model.getFullDate());
+                    let lastSymptomsCheckNumSymptoms = JSON.parse(localStorage.getItem("lastSymptomsCheckResults")).length;
+                    let lastSymptomsCheckDate = localStorage.getItem("lastSymptomsCheckDate");
+                    let symptomsCheckHistory = JSON.parse(localStorage.getItem("symptomsCheckHistory"));
+                    let newHistoryObject = {"numberOfSymptoms": lastSymptomsCheckNumSymptoms, "date": lastSymptomsCheckDate};
+                    symptomsCheckHistory.unshift(newHistoryObject);
+                    //If history has more than 5 elements, remove the last element in the history
+                    if (symptomsCheckHistory.length > 5) {
+                        symptomsCheckHistory.pop();
+                    }
+                    localStorage.setItem("symptomsCheckHistory", JSON.stringify(symptomsCheckHistory));
+                }
+
+                //Display results
+                if (isFormInputPositive()) {
+                    //Display positive results
+                    model.hideDiv(symptomsFormDiv);
+                    model.showDiv(symptomsResultsDiv);
+                    model.showDiv(positiveResultsDiv);
+                } else {
+                    //Display negative results
+                    model.hideDiv(symptomsFormDiv);
+                    model.showDiv(symptomsResultsDiv);
+                    model.showDiv(negativeResultsDiv);
+                }
+            } else {
+                //Invalid input
+                invalidInput.textContent = "You must answer every question before submitting.";
+            }
+        });
+
+        clearButton.addEventListener("click", () => {
+            clearFormInput();
+        });
+    }
+
+    //Details Page
+    if (document.URL.includes("profiledetails.html")) {
+        const noChecksDiv = document.getElementById("display-nochecks");
+        const positiveDetailsDiv = document.getElementById("display-positive-details");
+        const negativeDetailsDiv = document.getElementById("display-negative-details");
+        const previousChecksDiv = document.getElementById("display-checks-history");
+        const symptomsHistoryDiv = document.getElementById("symptoms-history");
+        const eraseDataBtn = document.getElementById("erase-data-btn");
+
+        const displaySymptomsHistory = function () {
+            let symptomsCheckHistory = JSON.parse(localStorage.getItem("symptomsCheckHistory"));
+            for (let i = 0; i < symptomsCheckHistory.length; i++) {
+                let numSymptoms = symptomsCheckHistory[i].numberOfSymptoms;
+                let date = symptomsCheckHistory[i].date;
+
+                //Create h1 element
+                let h1 = document.createElement("h1");
+                h1.classList.add("settings-title");
+                if (numSymptoms > 0) {
+                    h1.classList.add("negative-symptom");
+                } else {
+                    h1.classList.add("positive-symptom");
+                }
+                h1.textContent = `${numSymptoms} symptoms`;
+
+                //Create p element
+                let p = document.createElement("p");
+                p.classList.add("last-checked-date");
+                p.textContent = `${date}`;
+
+                //Add elements to div
+                symptomsHistoryDiv.append(h1);
+                symptomsHistoryDiv.append(p);
+
+                //Add hr unless the element is the last element in the list
+                if (i !== symptomsCheckHistory.length - 1) {
+                    let hr = document.createElement("hr");
+                    symptomsHistoryDiv.append(hr);
+                }
+            }
+        };
+
+        //Display appropriate content
+        if (localStorage.getItem("lastSymptomsCheckDate") === null) {
+            model.hideDiv(previousChecksDiv);
+            model.showDiv(noChecksDiv);
+        } else if (JSON.parse(localStorage.getItem("lastSymptomsCheckResults")).length === 0) {
+            document.querySelector(".positive-last-checked").textContent = `Last checked: ${localStorage.getItem("lastSymptomsCheckDate")}`;
+            displaySymptomsHistory();
+            model.showDiv(positiveDetailsDiv);
+        } else {
+            document.getElementById("num-of-symptoms").textContent = `${JSON.parse(localStorage.getItem("lastSymptomsCheckResults")).length} symptoms`;
+            //Add user's symptoms to symptoms-list div
+            const symptomsList = document.getElementById("symptoms-list");
+            for (let symptom of JSON.parse(localStorage.getItem("lastSymptomsCheckResults"))) {
+                let listElement = document.createElement("LI");
+                listElement.classList.add(".symptoms-li");
+                listElement.textContent = symptom;
+                symptomsList.append(listElement);
+            }
+            //Add symptoms check date to div
+            document.querySelector(".negative-last-checked").textContent = `Last checked: ${localStorage.getItem("lastSymptomsCheckDate")}`;
+            displaySymptomsHistory();
+            model.showDiv(negativeDetailsDiv);
+        }
+
+        //Event Listeners
+        eraseDataBtn.addEventListener("click", () => {
+            localStorage.removeItem("lastSymptomsCheckDate");
+            localStorage.removeItem("lastSymptomsCheckResults");
+            localStorage.removeItem("symptomsCheckHistory");
+            model.hideDiv(positiveDetailsDiv);
+            model.hideDiv(negativeDetailsDiv);
+            model.hideDiv(previousChecksDiv);
+            model.showDiv(noChecksDiv);
+        });
+    }
+
     //Stats Page
     if (document.URL.includes("statistics.html")) {
         const nationwideStatsButton = document.getElementById("nationwideStatsBtn");
@@ -129,7 +328,7 @@ const initialise = evt => {
 
         refreshBtn.addEventListener("click", () => {
             model.refreshLocation();
-        })
+        });
 
         dailySymptomsCheckSettingBtn.addEventListener("click", () => {
             model.toggleSettingEnabledOrDisabled(dailySymptomsCheckSettingText, dailySymptomsCheckSettingBtn);
