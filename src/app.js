1f7de9e74e55e5baf8af157ca635f04ea93fd4f0
Hares mahmood <hares.mahmood.2018@uni.strath.ac.uk>
2021-03-19 11:21:42 +0000

    Fixed issue with bottom navigation bar.

--- src/app.js
+++ src/app.js
@@ -9,70 +9,76 @@ $(document).ready(function(){
     $("#news-main").hide();
 
     $("#profile-link").click(function() {
-        counter = 1;
-        
-        $("#stats-main").fadeOut(300);
-        $("#news-main").fadeOut(300);
-        $("#profile-main").fadeIn(300);
+        if (counter !== 1) {
+            counter = 1;
+            
+            $("#stats-main").fadeOut(300);
+            $("#news-main").fadeOut(300);
+            $("#profile-main").fadeIn(300);
 
-        $("#stats-header").fadeOut(300);
-        $("#tabs-header").fadeOut(300);
-        $("#profile-header").delay(300).fadeIn(300);
+            $("#stats-header").fadeOut(300);
+            $("#tabs-header").fadeOut(300);
+            $("#profile-header").delay(300).fadeIn(300);
 
-        $("#profile-link").append("<p class='accent__color'>For you</p>");
-        $("#profile-link span").addClass("accent__color");
+            $("#profile-link").append("<p class='accent__color'>For you</p>");
+            $("#profile-link span").addClass("accent__color");
 
-        $("#stats-link").children().not("span").remove()
-        $("#stats-link span").removeClass("accent__color");
+            $("#stats-link").children().not("span").remove()
+            $("#stats-link span").removeClass("accent__color");
 
-        $("#news-link").children().not("span").remove()
-        $("#news-link span").removeClass("accent__color");
+            $("#news-link").children().not("span").remove()
+            $("#news-link span").removeClass("accent__color");
+        }
     });
 
     $("#stats-link").click(function() {
-        counter = 2;
-        
-        $("#profile-main").fadeOut(300);
-        $("#news-main").fadeOut(300);
-        $("#stats-main").fadeIn(300);
-        $("#nationwide-stats").hide()
+        if (counter !== 2) {
+            counter = 2;
+            
+            $("#profile-main").fadeOut(300);
+            $("#news-main").fadeOut(300);
+            $("#stats-main").fadeIn(300);
+            $("#nationwide-stats").hide()
 
-        $("#profile-header").fadeOut(300);
-        $("#stats-header").delay(300).fadeIn(300);
-        $("#tabs-header").fadeIn(300);
-        $("#nationwide-tab").trigger("click");
+            $("#profile-header").fadeOut(300);
+            $("#stats-header").delay(300).fadeIn(300);
+            $("#tabs-header").fadeIn(300);
+            $("#nationwide-tab").trigger("click");
 
-        $("#stats-link").append("<p class='accent__color'>Statistics</p>");
-        $("#stats-link span").addClass("accent__color");
+            $("#stats-link").append("<p class='accent__color'>Statistics</p>");
+            $("#stats-link span").addClass("accent__color");
 
-        $("#profile-link").children().not("span").remove()
-        $("#profile-link span").removeClass("accent__color");
+            $("#profile-link").children().not("span").remove()
+            $("#profile-link span").removeClass("accent__color");
 
-        $("#news-link").children().not("span").remove()
-        $("#news-link span").removeClass("accent__color");
+            $("#news-link").children().not("span").remove()
+            $("#news-link span").removeClass("accent__color");
+        }
     });
 
     $("#news-link").click(function() {
-        counter = 3;
+        if (counter !== 3) {
+            counter = 3;
 
-        $("#profile-main").fadeOut(300);
-        $("#stats-main").fadeOut(300);
-        $("#news-main").fadeIn(300);
-        $("#nationwide-news").hide()
+            $("#profile-main").fadeOut(300);
+            $("#stats-main").fadeOut(300);
+            $("#news-main").fadeIn(300);
+            $("#nationwide-news").hide()
 
-        $("#profile-header").fadeOut(300);
-        $("#stats-header").fadeOut(300);
-        $("#tabs-header").delay(300).fadeIn(300);
-        $("#nationwide-tab").trigger("click");
+            $("#profile-header").fadeOut(300);
+            $("#stats-header").fadeOut(300);
+            $("#tabs-header").delay(300).fadeIn(300);
+            $("#nationwide-tab").trigger("click");
 
-        $("#news-link").append("<p class='accent__color'>Statistics</p>");
-        $("#news-link span").addClass("accent__color");
+            $("#news-link").append("<p class='accent__color'>News</p>");
+            $("#news-link span").addClass("accent__color");
 
-        $("#profile-link").children().not("span").remove()
-        $("#profile-link span").removeClass("accent__color");
+            $("#profile-link").children().not("span").remove()
+            $("#profile-link span").removeClass("accent__color");
 
-        $("#stats-link").children().not("span").remove()
-        $("#stats-link span").removeClass("accent__color");
+            $("#stats-link").children().not("span").remove()
+            $("#stats-link span").removeClass("accent__color");
+        }
     });
 
     $("#nationwide-tab").click(function() {
