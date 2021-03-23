997128488ebda9a9b19558ccb6938546bdb26e18
Hares mahmood <hares.mahmood.2018@uni.strath.ac.uk>
2021-03-23 14:26:22 +0000

    Add navbar to animations.

--- src/loadAnimation.js
+++ src/loadAnimation.js
@@ -3,9 +3,11 @@
 $(document).ready(function() {
     $("header").fadeIn(300);
     $("main").fadeIn(300); 
+    $("nav").fadeIn(300);
 });
 
 window.onbeforeunload = function() {
     $("header").fadeOut(50);
     $("main").fadeOut(50);  
+    $("nav").fadeOut(50);
 }
